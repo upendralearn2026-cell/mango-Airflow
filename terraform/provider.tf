18219646daef89eeb835eaa9f463c8fe908f42fe
Upendra Dhamankar <upendralearn2026@gmail.com>
2026-02-15 16:54:07 +0530

    new updates

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "your-terraform-state-bucket"
+    bucket = "upd-mango-terraform-state-bucket"
     key    = "airflow-eks/terraform.tfstate"
     region = "us-east-1"
   }
